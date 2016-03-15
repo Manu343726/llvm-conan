b75b5947861c6b7e1c558f4bcf746e39def4e1be
Sam Spilsbury <smspillaz@gmail.com>
2016-03-15 20:40:46 +0800

    build: set pure_c to true, which avoids settings.compiler.libcxx being set

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=False)
+    builder.add_common_builds(pure_c=True)
     builder.run()
