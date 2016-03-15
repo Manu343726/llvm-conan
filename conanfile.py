cc03939e8be657eaf6217d5717787cda5d6f4907
Sam Spilsbury <smspillaz@gmail.com>
2016-03-15 20:25:08 +0800

    conan: Don't depend on settings.libcxx

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,7 @@ class LLVMConan(ConanFile):
     requires = tuple()
     url = "http://github.com/smspillaz/llvm-conan"
     license = "MIT"
-    settings = "os", "compiler", "build_type", "arch", "libcxx"
+    settings = "os", "compiler", "build_type", "arch"
 
     def config(self):
         try:  # Try catch can be removed when conan 0.8 is released
