09193aac6cdaefe926b67fdd6c6fa4855a633d77
Sam Spilsbury <smspillaz@gmail.com>
2016-03-17 18:26:09 +0800

    build: Add builds for linux that we know work

--- build.py
+++ build.py
@@ -1,7 +1,15 @@
 from conan.packager import ConanMultiPackager
-
+import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=True)
+
+    if platform.system() == "Linux":
+        for ver in ["4.8", "4.9" "5.2", "5.3"]:
+            for arch in ["x86", "x86_64"]:
+                builder.add({"arch": arch,
+                             "build_type": "Release",
+                             "compiler": "gcc",
+                             "compiler.version": ver},
+                             {})
     builder.run()
