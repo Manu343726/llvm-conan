cd29178bd6cc9b32d66b112756ce37915cee7304
Sam Spilsbury <smspillaz@gmail.com>
2016-03-18 20:29:53 +0800

    build: Don't attempt to build every version

--- build.py
+++ build.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
         builder.add_common_builds(visual_versions=[12, 13])
 
     if platform.system() == "Linux":
-        for ver in ["4.8", "4.9", "5.2", "5.3"]:
+        for ver in (filter(None,
+                          os.getenv("CONAN_GCC_VERSIONS", "").split(","))
+                    or ["4.8", "4.9", "5.2", "5.3"]):
             for arch in ["x86", "x86_64"]:
                 builder.add({"arch": arch,
                              "build_type": "Release",
@@ -16,11 +18,12 @@ if __name__ == "__main__":
                              "compiler.version": ver}, {})
 
     if platform.system() == "Darwin":
-        for compiler_version in ["5.0", "5.1", "6.0", "6.1", "7.0"]:
+        for ver in (filter(None,
+                          os.getenv("CONAN_APPLE_CLANG_VERSIONS_VERSIONS", "").split(","))
+                    or ["5.0", "5.1", "6.0", "6.1", "7.0"]):
             for arch in ["x86", "x86_64"]:
-                for build_type in ["Release"]:
-                    builder.add({"arch": arch,
-                                 "build_type": build_type,
-                                 "compiler": "apple-clang",
-                                 "compiler.version": compiler_version}, {})
+                builder.add({"arch": arch,
+                             "build_type": build_type,
+                             "compiler": "apple-clang",
+                             "compiler.version": compiler_version}, {})
     builder.run()
