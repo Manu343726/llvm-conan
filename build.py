46bbf7c562f6c51f41536a2a2b771c0b612dbf90
Sam Spilsbury <smspillaz@gmail.com>
2016-03-18 20:39:54 +0800

    build: Fix undefined variables

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 from conan.packager import ConanMultiPackager
 import platform
+import os
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(visual_runtimes=["MT", "MD"])
@@ -9,7 +10,7 @@ if __name__ == "__main__":
 
     if platform.system() == "Linux":
         for ver in (filter(None,
-                          os.getenv("CONAN_GCC_VERSIONS", "").split(","))
+                           os.getenv("CONAN_GCC_VERSIONS", "").split(","))
                     or ["4.8", "4.9", "5.2", "5.3"]):
             for arch in ["x86", "x86_64"]:
                 builder.add({"arch": arch,
@@ -19,11 +20,11 @@ if __name__ == "__main__":
 
     if platform.system() == "Darwin":
         for ver in (filter(None,
-                          os.getenv("CONAN_APPLE_CLANG_VERSIONS_VERSIONS", "").split(","))
+                           os.getenv("CONAN_APPLE_CLANG_VERSIONS_VERSIONS", "").split(","))
                     or ["5.0", "5.1", "6.0", "6.1", "7.0"]):
             for arch in ["x86", "x86_64"]:
                 builder.add({"arch": arch,
-                             "build_type": build_type,
+                             "build_type": "Release",
                              "compiler": "apple-clang",
-                             "compiler.version": compiler_version}, {})
+                             "compiler.version": ver}, {})
     builder.run()
